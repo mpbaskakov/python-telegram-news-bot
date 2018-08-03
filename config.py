13a9a7d5da1a088286f2411f55d40340c56897c7
Maxim Baskakov <mpbaskakov@gmail.com>
2018-08-03 15:45:19 +0300

    Initial commit

---
+++ config.py
@@ -0,0 +1,10 @@
+import os
+
+token = os.environ['TOKEN_NEWS_BOT']
+post_channel = os.environ['POST_CHANNEL']
+forecast_url = os.environ['FORECAST_URL']
+digest_url = os.environ['DIGEST_URL']
+digest_name = os.environ['DIGEST_NAME']
+proxy_url = os.environ['PROXY_URL']
+proxy_login = os.environ['PROXY_LOGIN']
+proxy_password = os.environ['PROXY_PASSWORD']
--- vrnbot.py
+++ vrnbot.py
@@ -3,6 +3,8 @@ import logging
 from telegram.ext import Updater, CommandHandler
 import requests
 from bs4 import BeautifulSoup
+import config
+
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
@@ -37,34 +39,81 @@ def get_forecast(html):
     return fc
 
 
+def check_posted(text):
+    with open('last_posted.txt', 'r') as f:
+        if text == f.read():
+            return True
+        else:
+            return False
+
+
+def write_posted(text):
+    with open('last_posted.txt', 'w') as f:
+        f.write(text)
+
+
+def get_downtown_digest(html):
+    # TODO: not less than 3 news
+    soup = BeautifulSoup(html, 'lxml')
+    dd_list = soup.find_all('item')
+    post_text = '*Дайджест {}:*\n'.format(config.digest_name)
+    for item in dd_list:
+        if check_posted(item.find('title').text):
+            dd_list = dd_list[:dd_list.index(item)]
+            break
+    if not dd_list:
+        return None
+    count = 1
+    for item in dd_list[::-1]:
+        title = item.find('title').text
+        link = item.find('guid').text
+        post_text += ('{}) [{}]({})\n'.format(count, title, link))
+        count += 1
+        write_posted(title)
+    return post_text
+
+
 def post_downtown_digest(bot, update):
-    pass
+    dd = get_downtown_digest(get_html(config.digest_url))
+    if not dd:
+        return
+    bot.send_message(config.post_channel, dd, parse_mode='Markdown')
 
 
 def post_yandex_digest(bot, update):
     pass
 
 
-def post_forecast(bot, update):
-    fc = get_forecast(get_html('https://export.yandex.ru/bar/reginfo.xml?region=193'))
-    post_text = 'Доброе утро, {}!\n\n'.format(fc['city'])
-    post_text += 'Сегодня {}.\n'.format(fc['traffic'])
-    post_text += 'Восход солнца: *{}*, заход: *{}*'.format(fc['sunrise'], fc['sunset'])
-    post_text += '\nt° утром: *{}*'.format(fc['morning_temp'])
-    post_text += '\nt° днем: *{}*'.format(fc['day_temp'])
-    post_text += '\nt° вечером: *{}*'.format(fc['evening_temp'])
-    post_text += '\nt° ночью: *{}*'.format(fc['night_temp'])
-    bot.send_message('@vrn_news', post_text, parse_mode='Markdown')
+def post_forecast(bot, update, job):
+    fc = get_forecast(get_html(config.forecast_url))
+    common_text = 'Пробки: *{}*.\nВосход солнца: *{}*, заход: *{}*'.format(fc['traffic'], fc['sunrise'], fc['sunset'])
+    t_morning = '\nt° утром: *{}*\nt° днем: *{}*'.format(fc['morning_temp'], fc['day_temp'])
+    t_evening = '\nt° вечером: *{}*\nt° ночью: *{}*'.format(fc['evening_temp'], fc['night_temp'])
+    if job.context == 'morning':
+        post_text = 'Доброе утро, {}!\n\n'.format(fc['city']) + common_text + t_morning + t_evening
+    else:
+        post_text = 'Добрый вечер, {}!\n\n'.format(fc['city']) + common_text + t_evening + t_morning
+    bot.send_message(config.post_channel, post_text, parse_mode='Markdown')
+
+
+def post_forecast_now(bot, update):
+    #TODO: make adaptive
+    fc = get_forecast(get_html(config.forecast_url))
+    common_text = 'Пробки: *{}*.\nВосход солнца: *{}*, заход: *{}*'.format(fc['traffic'], fc['sunrise'], fc['sunset'])
+    t_morning = '\nt° утром: *{}*\nt° днем: *{}*'.format(fc['morning_temp'], fc['day_temp'])
+    t_evening = '\nt° вечером: *{}*\nt° ночью: *{}*'.format(fc['evening_temp'], fc['night_temp'])
+    post_text = 'Доброе утро, {}!\n\n'.format(fc['city']) + common_text + t_morning + t_evening
+    bot.send_message(config.post_channel, post_text, parse_mode='Markdown')
 
 
 def main():
     """Start the bot."""
     # Create the EventHandler and pass it your bot's token.
-    updater = Updater("316755740:AAFk_rJg20gk7sMonMpxSkFAKiJTz6noZmY",
-                      request_kwargs={'proxy_url': 'socks5h://81.2.244.181:7007',
+    updater = Updater(config.token,
+                      request_kwargs={'proxy_url': config.proxy_url,
                                       'urllib3_proxy_kwargs': {
-                                                                'username': 'tgsocks',
-                                                                'password': 'tgpass',
+                                                                'username': config.proxy_login,
+                                                                'password': config.proxy_password,
                                                                 }
                                       })
 
@@ -72,13 +121,14 @@ def main():
     dp = updater.dispatcher
 
     # on different commands - answer in Telegram
-    dp.add_handler(CommandHandler("forecast", post_forecast))
+    dp.add_handler(CommandHandler("forecast", post_forecast_now))
 
     # log all errors
     dp.add_error_handler(error)
     job_queue = updater.job_queue
-    job = job_queue.run_daily(post_forecast, time=datetime.time(hour=9), name='forecast')
-    #job_dgst = job_queue.run_daily(post_downtown_digest, time=datetime.time(hour=19), first=0, name='digest')
+    job_morning = job_queue.run_daily(post_forecast, time=datetime.time(hour=7), name='forecast', context='morning')
+    job_evening = job_queue.run_daily(post_forecast, time=datetime.time(hour=17), name='forecast', context='evening')
+    job_dgst = job_queue.run_repeating(post_downtown_digest, 14400, first=0, name='digest')
     updater.start_polling()
     # Run the bot until you press Ctrl-C or the process receives SIGINT,
     # SIGTERM or SIGABRT. This should be used most of the time, since
